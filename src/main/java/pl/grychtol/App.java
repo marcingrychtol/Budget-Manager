d61cd44a1acb54c1d0891c689ec98dca63f949b1
marcingrychtol <mgmarcingrychtol@users.noreply.github.com>
2019-11-27 20:13:52 +0100

    przerzucenie funkcji menu

--- src/main/java/pl/grychtol/App.java
+++ src/main/java/pl/grychtol/App.java
@@ -36,6 +36,48 @@ public class App {
         System.out.println("Total: $"+summary);
 
     }
+    public static void showMenu() {
+        System.out.println("1) Add income");
+        System.out.println("2) Add purchase");
+        System.out.println("3) Show list of purchases");
+        System.out.println("4) Balance");
+        System.out.println("0) Exit");
+    }
+
+    private static int tempInput;
+
+    public static void readInput(String input) {
+        if (input.isEmpty()) {
+            return;
+        }
+        tempInput = Integer.parseInt(input);
+        switch (tempInput) {
+            case 0:{
+
+                break;
+            }
+            case 1:{
+
+                break;
+            }
+            case 2:{
+
+                break;
+            }
+            case 3:{
+
+                break;
+            }
+            case 4:{
+
+                break;
+            }
+            default:{
+
+                return;
+            }
+        }
+    }
 
 
 }
